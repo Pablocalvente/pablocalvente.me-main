030517b3e5730ef62dade181d7e6693f36239f62
Pablocalvente <pablocalventegomeztrabajo@gmail.com>
2025-02-02 14:48:53 +0100

    Fix: Ajuste en rutas y nombres de archivos para compatibilidad con Netlify

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,15 @@
-import HeroSection from "@/es/components/HeroSection";
-import Navbar from "@/es/components/Navbar";
-import AboutSection from "@/es/components/AboutSection";
-import ProjectsSection from "@/es/components/ProjectsSection";
-import EmailSection from "@/en/components/EmailSection";
-import Footer from "@/components/Footer";
-import AchievementsSection from "@/es/components/AchievementsSection";
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col bg-[#121212] overflow-hidden">
-      <Navbar />
-      <div className="container mt-14 mx-auto px-6 py-4 overflow-hidden">
-        <HeroSection />
-        {/* <AchievementsSection /> */}
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
-  );
+    const router = useRouter();
+
+    useEffect(() => {
+        // Redirige automáticamente a /es cuando se accede a /
+        router.push('/es');
+    }, [router]);
+
+    return null;  // No renderiza nada, solo realiza la redirección
 }
